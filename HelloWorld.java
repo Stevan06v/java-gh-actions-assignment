a8fb05667bf1600d1bdb2baf9b0ea2984e367ee6
stevan06v <stevanvlajic5@gmail.com>
2023-10-04 22:07:25 +0200

    fix: fixed build.yaml

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld{
     public static void main(String[] args) {
-        System.out.println("Hello form actions");
+        System.out.println("Hello World from GitHub-Actions by Stevan Vlajic");
     }
 }
 
